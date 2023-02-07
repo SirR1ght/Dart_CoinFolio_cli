b1b6acc235b8aab03a6068331b78fed144cba98f
SirR1ght <sirdavydov@gmail.com>
2023-02-07 20:29:17 +0300

    added emojis

--- coinfolio/lib/tools.dart
+++ coinfolio/lib/tools.dart
@@ -1,6 +1,8 @@
+
 const endpointPing = "https://api.binance.com/api/v3/ping";
 const endpointPrice = "https://api.binance.com/api/v3/ticker/price";
 const endpoint24hr = "https://api.binance.com/api/v3/ticker/24hr";
+const endpointTime = "https://api.binance.com/api/v3/time";
 
 String urlGet(List<String> list) {
 	if (list.length == 1) {
@@ -18,16 +20,3 @@ String urlGet(List<String> list) {
 	return endpointPrice;
 }
 
-
-// void print_intro() {
-
-// 	stdout.write('|');
-// 	print ('******************************************************\n'
-// 		'*~~~           Welcome to CoinFolio           ~~~*\n'
-// 		'*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\n'
-// 		'*~~~         You can use these commands:           ~~~*\n'
-// 		'*~~~      SEARCH - to look for current assets value ~~~*\n'
-// 		'*~~~      LIST    - to display all portfolio          ~~~*\n'
-// 		'*~~~      EXIT   - to quit                        ~~~*\n'
-// 		'******************************************************');
-// }
